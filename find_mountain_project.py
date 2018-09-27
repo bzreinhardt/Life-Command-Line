e5646000ca2947807fb3e0d2dcda62822d2c0414
bzreinhardt <bzreinhardt@gmail.com>
2018-09-26 21:43:49 -0700

    add upload picture, find mountain project

---
+++ find_mountain_project.py
@@ -0,0 +1,74 @@
+'''
+To use this script install:
+pip install pyperclip
+(Other things should be in conda)
+usage:
+python compose_intro_email.py firstname1 lastname1 firstname2 lastname2
+Works best if you use full names not nicknames
+Based on: https://gist.github.com/erogol/6658881
+Benjamin Reinhardt benjaminreinhardt.com July 2018
+The pro move is to add this to your bash_profile
+alias intro='python <path>/compose_intro_email.py'
+'''
+import pyperclip
+import json
+import pdb
+import urllib
+import mechanize
+import cookielib
+import sys
+import os
+from BeautifulSoup import BeautifulSoup
+import webbrowser
+
+#Todo: turn name into query string
+#Template for email
+# Stick linkedin in name
+
+def find_mountain(query):
+    query = query.replace(' ','+')
+    br = mechanize.Browser()
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
+
+    # Browser options
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+    # Want debugging messages?
+    #br.set_debug_http(True)
+    #br.set_debug_redirects(True)
+    #br.set_debug_responses(True)
+
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+    main_url = 'https://www.google.com.tr/search?q='+query
+    print(main_url)
+    r = br.open(main_url)
+    html = r.read()
+    soup = BeautifulSoup(html)
+    links = []
+    for link in soup.findAll('a'):
+        href = link.get('href')
+        if type(href) is unicode:
+            if 'mountainproject' in href:
+                    links.append(href)
+    start = links[0].find('https')
+    stop = links[0].find('&')
+    linkedin = links[0][start:stop]
+    return linkedin
+
+
+
+if __name__ == '__main__':
+        name = ' '.join(sys.argv[1:])
+        mountain = find_mountain(name)
+        print("%s copied to clipboard!" % mountain)
+        pyperclip.copy(str(mountain))
---
+++ upload_picture_to_s3.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+S3_BUCKET                 = "zaaron-personal"
+S3_KEY                    = os.environ.get("AWSAccessKeyId")
+S3_SECRET                 = os.environ.get("AWSSecretKey")
+S3_LOCATION               = 'https://s3-us-west-1.amazonaws.com/{}'.format(S3_BUCKET)
+
+SECRET_KEY                = os.urandom(32)
+DEBUG                     = True
+PORT                      = 5000
+
+import boto3, botocore
+
+
+def upload_file_to_s3(file, bucket_name, acl="public-read"):
+    print(file)
+    try:
+        s3.upload_file(
+            file,
+            bucket_name,
+            os.path.basename(file),
+            ExtraArgs={'ACL': 'public-read',
+                       'ContentType':'image'}
+        )
+
+    except Exception as e:
+        # This is a catch all exception, edit this part to fit your needs.
+        print("Something Happened: ", e)
+        return e
+
+    return "{}/{}".format(S3_LOCATION,  os.path.basename(file).replace(" ", "+"))
+
+
+if __name__== '__main__':
+    print("hello")
+    print(sys.argv)
+    file = os.path.abspath(sys.argv[1])
+    s3 = boto3.client(
+       "s3",
+       aws_access_key_id=S3_KEY,
+       aws_secret_access_key=S3_SECRET
+    )
+
+    url = upload_file_to_s3(file, S3_BUCKET)
+    print(url)
